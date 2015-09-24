6211a35cbb0f6abc7845e7a30425125f204d6ac8
gigabit101 <thegigabit101@gmail.com>
2015-09-24 22:49:35 +0100

    version bump

--- src/main/java/BuildcentricTweeks/lib/ModInfo.java
+++ src/main/java/BuildcentricTweeks/lib/ModInfo.java
@@ -4,5 +4,5 @@ public class ModInfo
 {
 	public static final String MOD_ID = "BuildcentricTweeks";
 	public static final String MOD_NAME = "BuildcentricTweeks";
-	public static final String MOD_VERSION = "1.0.1";
+	public static final String MOD_VERSION = "1.0.2";
 }
